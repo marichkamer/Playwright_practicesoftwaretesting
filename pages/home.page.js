c3b351a93f59ec2b44d6d63861653f5ba5740ba9
Marichka Merza <merzamarichka@gmail.com>
2026-08-28 17:39:23 +0300

    fix stealth.4

--- pages/home.page.js
+++ pages/home.page.js
@@ -1,4 +1,4 @@
-import { BasePage } from './base.page';
+import { BasePage } from './base.page.js';
 export class HomePage extends BasePage {
   constructor(page) {
     super(page);
--- pages/login.page.js
+++ pages/login.page.js
@@ -1,4 +1,4 @@
-import { BasePage } from './base.page';
+import { BasePage } from './base.page.js';
 
 export class LoginPage extends BasePage {
   constructor(page) {
